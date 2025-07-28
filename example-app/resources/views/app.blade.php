af349848599b6df67c60bdb5aed61123aa4f749b
janka000 <14cernikova@gmail.com>
2025-07-28 15:58:22 +0200

    cleanup

--- example-app/resources/views/app.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout')
-
-@section('content')
-<div class="container mt-5">
-    <a href="{{ route('posts.index') }}" class="btn btn-success mb-3">Posts</a>
-    <a href="{{ route('todo.index') }}" class="btn btn-info mb-3">Todos</a>
-</div>
-@endsection
